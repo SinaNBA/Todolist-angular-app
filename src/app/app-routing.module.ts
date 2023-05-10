dafce6d1f9b666f0c62cef6568d2699db2fbb5fa
Sina Nejadbakhtiari <snejadbakhtiari@gmail.com>
2023-05-10 16:25:30 +0200

    Adds PageNotFound to Project(WildCard Routing)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ListComponent } from './list/list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'list', component: ListComponent },
+  { path: '**', component: PageNotFoundComponent },
 ];
 
 @NgModule({
